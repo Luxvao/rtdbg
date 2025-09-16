b8988373dfd376dda7eda215889e76b436cb543c
Bor Grofelnik <luxvoo.me@gmail.com>
2025-09-16 18:25:24 +0200

    Nix stuff

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A flake for mmux";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+    in
+    {
+      devShells.default = with pkgs; mkShell {
+        buildInputs = [
+          pkg-config
+          eza
+          fish
+          rust-bin.stable.latest.default
+          rust-analyzer
+        ];
+
+        shellHook = ''
+          fish
+        '';
+      };
+    }
+  );
+}
